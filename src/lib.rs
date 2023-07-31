4d6fd8b7c89d9175359618aa4666fc6199440507
djugei <ddjugei@gmail.com>
2023-07-31 18:40:52 +0200

    documentation added

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+//! Tired of your log lines and progress bars mixing up? indicatif_log_bridge to the rescue!
+//!
+//! Simply wrap your favourite logging implementation in [crate::LogWrapper]
+//!     and those worries are a thing of the past.
+//!
+//! Just remember to only use progress bars added to the [MultiProgress] you used
+//!     , otherwise you are back to ghostly halves of progress bars everywhere.
+//!
+//! # Example
+//! ```rust
+//!     # use indicatif_log_bridge::LogWrapper;
+//!     # use log::info;
+//!     # use indicatif::{MultiProgress, ProgressBar};
+//!     # use std::time::Duration;
+//!     let logger =
+//!         env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+//!             .build();
+//!     let multi = MultiProgress::new();
+//!
+//!     LogWrapper::new(multi.clone(), logger)
+//!         .try_init()
+//!         .unwrap();
+//!
+//!     let pg = multi.add(ProgressBar::new(10));
+//!     for i in (0..10) {
+//!         std::thread::sleep(Duration::from_micros(100));
+//!         info!("iteration {}", i);
+//!         pg.inc(1);
+//!     }
+//!     pg.finish();
+//!     multi.remove(&pg);
+//! ```
+//! The code of this crate is pretty simple, so feel free to check it out.
+
 use indicatif::MultiProgress;
 use log::Log;
 
